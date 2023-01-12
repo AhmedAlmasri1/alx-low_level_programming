28f17414f943a1082adaef254d621d5e568a2284
AhmedAlmasri1 <78304031+AhmedAlmasri1@users.noreply.github.com>
2023-01-12 23:35:34 +0200

    betty

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,33 +4,16 @@
 
 
 /**
-
  * malloc_checked - A function that allocates memory using malloc.
-
  * @b: An unsigned input integer
-
  * Return: Apointer to allocated memory or NULL if it fails
-
  */
 
 void *malloc_checked(unsigned int b)
-
 {
-
-	void *s;
-
-
-
-	s = malloc(b);
-
-
-
+void *s;
+s = malloc(b);
 	if (s == NULL)
-
 		exit(98);
-
-
-
 	return (s);
-
 }
